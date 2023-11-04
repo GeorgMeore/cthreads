edf0f7ad94570bf0ed2cc086c24892d13d051b3a
ggp <georgii.galushko@gmail.com>
2023-11-04 16:52:58 +0300

    use one build script

--- pthreads/buid.sh
+++ pthreads/buid.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-gcc -g -Wall -Wextra -lpthread -o main main.c
+case "$*" in
+	'') gcc -g -Wall -Wextra -lpthread -o main main.c ;;
+	-c) rm -f main ;;
+	*)
+		echo "usage: $0 [-c]"
+		exit 1
+esac
