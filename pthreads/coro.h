b5d2e4659ded463aa76936d9ac72ce6e9f607c8b
ggp <georgii.galushko@gmail.com>
2023-11-09 00:37:12 +0300

    a new channel implementation that supports correct closing

--- pthreads/coro.h
+++ pthreads/coro.h
@@ -3,18 +3,34 @@
 
 #include <pthread.h>
 
-#define CORO2(name, p1, p2)             \
-	struct name##args { p1; p2; };      \
-	void *name(struct name##args *args) \
-	{                                   \
-		p1; p2;                         \
+/* Some dirty macro tricks */
 
-#define SETARGS2(n1, n2) \
+#define CORO_BEGIN2(name, p1, p2) \
+struct name##args { p1; p2; };      \
+void *name(struct name##args *args) \
+{                                   \
+	p1; p2;
+
+#define CORO_BEGIN1(name, p1) \
+struct name##args { p1; };      \
+void *name(struct name##args *args) \
+{                                   \
+	p1;
+
+#define CORO_SETARGS1(n1) \
+	n1 = args->n1;       \
+	free(args);
+
+#define CORO_SETARGS2(n1, n2) \
 	n1 = args->n1;       \
 	n2 = args->n2;       \
 	free(args);
 
-#define START(name, argl...) {                                    \
+#define CORO_END \
+	return NULL; \
+}
+
+#define CORO_START(name, argl...) { \
 	pthread_t handle;                                             \
 	struct name##args *args = malloc(sizeof(*args));              \
 	*args = (struct name##args){argl};                            \
--- pthreads/main.c
+++ pthreads/main.c
@@ -4,12 +4,39 @@
 #include "chan.h"
 #include "coro.h"
 
-CORO2(foo, CHAN_OF(int) *in, CHAN_OF(int) *out)
+CORO_BEGIN1(printer, CHAN_OF(int) *in)
+	int x;
+	CORO_SETARGS1(in);
+	for (;;) {
+		int err = chrecv(in, &x);
+		if (err == CHAN_CLOSED)
+			break;
+		printf("%d\n", x);
+	}
+	printf("printer finished\n");
+CORO_END
+
+CORO_BEGIN1(sender, CHAN_OF(int) *out)
+	int x = 0;
+	CORO_SETARGS1(out);
+	for (;;) {
+		int err = chsend(out, &x);
+		if (err == CHAN_CLOSED)
+			break;
+		x += 1;
+	}
+	printf("sender finished\n");
+CORO_END
+
+CORO_BEGIN2(foo, CHAN_OF(int) *in, CHAN_OF(int) *out)
 	int x, y;
-	SETARGS2(in, out);
+	CORO_SETARGS2(in, out);
 	printf("starting a foo loop\n");
 	for (;;) {
-		if (chtryrecv(in, &x)) {
+		int err = chtryrecv(in, &x);
+		if (err == CHAN_CLOSED)
+			break;
+		if (err == CHAN_OK) {
 			printf("foo received: %d\n", x);
 			y = x + 10;
 			chsend(out, &y);
@@ -19,16 +46,17 @@ CORO2(foo, CHAN_OF(int) *in, CHAN_OF(int) *out)
 		}
 	}
 	printf("exited a foo loop\n");
-}
+	sleep(5);
+	chclose(out);
+CORO_END
 
-int main(void)
+int footest(void)
 {
 	chan in, out;
 	int x, y;
-	if (!CHAN_INIT(in, int) || !CHAN_INIT(out, int)) {
+	if (!chinit(&in, sizeof(int)) || !chinit(&out, sizeof(int)))
 		return 1;
-	}
-	START(foo, &in, &out);
+	CORO_START(foo, &in, &out);
 	printf("starting the main loop\n");
 	while (scanf(" %d", &x) == 1) {
 		chsend(&in, &x);
@@ -36,5 +64,28 @@ int main(void)
 		printf("%d\n", y);
 	}
 	printf("exited the main loop\n");
+	chclose(&in);
+	chrecv(&out, &y);
+	chdestroy(&in);
+	chdestroy(&out);
 	return 0;
+}
+
+int sendprinttest()
+{
+	chan c;
+	if (!chinit(&c, sizeof(int)))
+		return 1;
+	CORO_START(sender, &c);
+	CORO_START(printer, &c);
+	getchar();
+	chclose(&c);
+	getchar();
+	chdestroy(&c);
+	return 0;
+}
+
+int main(void)
+{
+	return sendprinttest();
 }
